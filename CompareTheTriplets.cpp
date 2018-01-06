dcaf0b706dcdaa759cdf92a2f0ae1a357b447436
dax2367 <34932414+dax2367@users.noreply.github.com>
2018-01-06 00:54:05 -0800

    Update CompareTheTriplets.cpp

--- CompareTheTriplets.cpp
+++ CompareTheTriplets.cpp
@@ -65,21 +65,21 @@ int main() {
         suma++;
     } else if (b0>a0){
         sumb++;
-   }
+    }
     
    if(a1>b1){
         suma++;
     } else if (b1>a1){
         sumb++;
-   }
+    }
     
    if(a2>b2){
         suma++;
     } else if (b2>a2){
         sumb++;
-   }
+    }
    
     cout << suma << " " << sumb;
     
-    }
+}
 
