704e1f42813070815b602fd96d55d5ba8b3ac983
dax2367 <34932414+dax2367@users.noreply.github.com>
2018-01-08 14:18:10 -0800

    Update C++ Folder

---
+++ C++/00_Introduction/00_Say_Hello_World_With_C++/SayHelloCpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout<<("Hello, World!");
+
+	
+	//keep console window open when ran in GUI
+	cin.ignore();
+	cin.get();
+
+
+    return 0;
+}
