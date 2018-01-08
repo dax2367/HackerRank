2fbdec1bce5325700f08754b57d25217101bc273
dax2367 <34932414+dax2367@users.noreply.github.com>
2018-01-08 14:32:28 -0800

    Add Input and Output Solution

---
+++ C++/00_Introduction/01_Input_and_Output/Input_and_Output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c;
+    cin>>a >>b >>c;
+    cout<<(a+b+c)<<endl;
+
+	//keep console window open when ran in GUI
+	cin.ignore();
+	cin.get();
+	
+    return 0;
+}
