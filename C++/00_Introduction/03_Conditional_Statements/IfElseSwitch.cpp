a429e11fbd4ce88d3aafde2f6977485b50db59b9
dax2367 <34932414+dax2367@users.noreply.github.com>
2018-01-08 15:41:10 -0800

    Removed Unnecessary Includes

--- C++/00_Introduction/03_Conditional_Statements/IfElseSwitch.cpp
+++ C++/00_Introduction/03_Conditional_Statements/IfElseSwitch.cpp
@@ -1,25 +1,4 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
@@ -27,7 +6,6 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    // your code goes here
     if (n>9)
         cout<<"Greater than 9";
     else
@@ -63,13 +41,6 @@ int main(){
           default:
             cout<<"The input is less than one";
             break;
-        }
-
-		//keep console window open when ran in GUI
-	cin.ignore();
-	cin.get();
-        
+        }		        
     return 0;
-
-
 }
